5df00c56f29b8b9f1ef7a0b0a4e60946148cf95f
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-03-26 15:57:56 +0500

    BROKERS: Create Api CRUD

--- Brokers/API/ApiBroker.cs
+++ Brokers/API/ApiBroker.cs
@@ -1,4 +1,5 @@
-﻿using RESTFulSense.Clients;
+﻿using System.Threading.Tasks;
+using RESTFulSense.Clients;
 
 namespace CashOverFlow.Portal.Brokers.API
 {
@@ -7,6 +8,17 @@ namespace CashOverFlow.Portal.Brokers.API
         private readonly IRESTFulApiFactoryClient apiClient;
         public ApiBroker(IRESTFulApiFactoryClient apiClient) =>
             this.apiClient = apiClient;
+
+        private async ValueTask<T> GetAsync<T>(string relativeUrl) =>
+            await this.apiClient.GetContentAsync<T>(relativeUrl);
+
+        private async ValueTask<T> PostAsync<T>(string relativeUrl, T content) =>
+            await this.apiClient.PostContentAsync<T>(relativeUrl, content);
         
+        private async ValueTask<T> PutAsync<T>(string relativeUrl, T content) =>
+            await this.apiClient.PutContentAsync<T>(relativeUrl, content);
+
+        private async ValueTask<T> DeleteAsync<T>(string relativeurl) =>
+            await this.DeleteAsync(relativeurl);
     }
 }
